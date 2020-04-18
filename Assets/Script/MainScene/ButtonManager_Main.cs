1367f2bc682aeb000427651097e221c7082f505f
SongHyunJi <songhj131109@gmail.com>
2020-04-19 01:37:28 +0900

    ui Setting
    My Info Scene, Main Scene UI Setting

---
+++ Assets/Script/MainScene/ButtonManager_Main.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager_Main : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void PressIslandBtn()
+	{
+		SceneManager.LoadScene("SampleScene");
+	}
+
+	public void PressPlayBtn()
+	{
+		SceneManager.LoadScene("SampleScene");
+	}
+
+	public void PressEglooBtn()
+	{
+		SceneManager.LoadScene("MyInfoScene");
+	}
+}
