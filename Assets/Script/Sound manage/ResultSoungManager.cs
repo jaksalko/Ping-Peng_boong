ddc63307c29e53200c7bbbcfa66cbe0fc44b3177
SongHyunJi <songhj131109@gmail.com>
2020-05-17 01:04:40 +0900

    Add Sound
    사운드 추가
    
    - bgm (lobby, in game)
    - resutl popup (clear 시)

---
+++ Assets/Script/Sound manage/ResultSoungManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultSoungManager : MonoBehaviour
+{
+	public AudioSource sfxSound;
+
+	GameObject baseCanvas;
+	GameObject setting;
+	Slider sfxVolumeSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		baseCanvas = GameObject.FindWithTag("BaseCanvas");
+		setting = baseCanvas.transform.GetChild(1).gameObject;
+		sfxVolumeSlider = setting.gameObject.transform.GetChild(5).GetComponent<Slider>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		sfxSound.volume = sfxVolumeSlider.value;
+
+	}
+}
---
+++ Assets/Script/Sound manage/SoundManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SoundManager : MonoBehaviour
+{
+	public AudioClip mainBGSound;
+	public AudioClip gameBGSound;
+
+	public int[] mainSoundScene;
+	public int[] gameSoundScene;
+
+	public Slider bgmVolumnSlider;
+
+	AudioSource audioSource;
+	int swtichScene = -1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		audioSource = gameObject.GetComponent<AudioSource>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		int index = SceneManager.GetActiveScene().buildIndex;
+		if (mainSoundScene.Contains(index) && swtichScene == -1)
+		{
+			audioSource.clip = mainBGSound;
+			audioSource.Play();
+			swtichScene = 0;
+		}
+		else if(gameSoundScene.Contains(index) && swtichScene == 0)
+		{
+			audioSource.clip = gameBGSound;
+			audioSource.Play();
+			swtichScene = -1;
+		}
+
+		audioSource.volume = bgmVolumnSlider.value;
+	}
+}
