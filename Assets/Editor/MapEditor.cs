7d7f96c072ff8f22e0d0f3aa44dd15928b0d8a56
정문경 <jaksal95@naver.com>
2020-06-11 23:35:51 +0900

    Map Generator

---
+++ Assets/Editor/MapEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MapGenerator))]
+public class MapEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        MapGenerator mapGenerator = (MapGenerator)target;
+
+        if(GUILayout.Button(""))
+        {
+            mapGenerator.Output();
+        }
+    }
+
+}
