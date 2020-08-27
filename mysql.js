810f6cf54a4cfb882cefac42f5a5b46b649aa6e9
jaksalko <ubuntu@ip-172-31-34-224.ap-northeast-2.compute.internal>
2020-08-27 10:46:15 +0000

    Initialize

---
+++ mysql.js
@@ -0,0 +1,187 @@
+var http = require("http");
+var express = require('express');
+var app = express();
+
+var mysql = require('mysql');
+/*var Mysql = require('mysql-json');
+var mysql = new Mysql({
+	host : '15.164.219.253',
+	user : 'admin',
+	password : 'admin0425',
+	dataabase : 'map'
+});*/
+
+var bodyParser = require('body-parser');
+
+
+var connection = mysql.createConnection(
+{
+	host : '15.164.219.253',
+	user : 'admin',
+	password : 'admin0425',
+	database : 'maplist'
+});
+
+
+
+connection.connect(function(err){
+	if(err) throw err
+	console.log('connection...');
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended : true
+}));
+
+var server = app.listen(3000, "0.0.0.0" , function(){
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log("example app listening at http://%s:%s" , host, port);
+});
+app.get('/count',function(req,res){
+	console.log(req);
+	connection.query('select count(*) from map',function(error,result,field)
+	{
+		if(error){console.log(error);}
+		res.end(JSON.stringify(result));
+	});
+});
+app.get('/map/difficulty' , function(req,res){
+	var param = req.query.difficulty;
+	var nickname = req.query.nickname;
+	var sql = 'select * from map where difficulty = ? and id != ?';
+	console.log("param : "+ param+"  nick : " + nickname);
+	connection.query(sql,[param,nickname],function(error, results, fields)
+	{	
+		if(error){console.log(error);}
+		console.log(results);
+		res.end(JSON.stringify(results));		
+	});
+});
+
+app.get('/test' , function(req,res){
+	console.log(req);
+	connection.query('select * from map',function(error, results, fields)
+	{	
+		if(error){console.log(error);}
+		res.end(JSON.stringify(results));		
+	});
+});
+app.post('/test',function(req,res){
+	var postData = req.body;
+	connection.query('INSERT INTO map SET ?', postData ,
+	 function(error,results,fields)
+	{
+		if(error){console.log(error);}
+		res.end(JSON.stringify(results));
+	});
+});
+
+app.get('/igloo/playerskin', function(req,res){
+	var userid = req.query.userid;
+	var sql = 'select * from skin where userid=?';
+	console.log(sql + userid);
+        connection.query(sql, userid, function(error, results, fields)
+        {
+                if(error){console.log(error);}
+                res.end(JSON.stringify(results));
+        });
+});
+app.post('/igloo/playerskin',function(req,res){
+	var userid = req.body.userid;
+        var skinid = req.body.skinid;
+	console.log(userid);
+	var existuser = false;
+	connection.query('select distinct userid from skin', function(error, rows, fields)
+	{
+		for(var i = 0; i < rows.length; i++)
+		{
+			console.log(rows[i]);
+			if(userid == rows[i].userid) {existuser = true;}
+		}
+		if(!existuser) {res.end("wrong user id");}
+	});
+
+	var key1 = "default";
+	var key2 = "default";
+	var rid = 0;
+	connection.query('select * from skin where userid=?', userid, function(error, rows, fields)
+	{
+		if(error) {console.log(error);}
+		console.log(rows);
+		for(var j = 0; j < rows.length; j++)
+		{
+			if(rows[j].skinid_1 == skinid ||
+				rows[j].skinid_2 == skinid ||
+				rows[j].skinid_3 == skinid ||
+				rows[j].skinid_4 == skinid ||
+				rows[j].skinid_5 == skinid)
+			{
+				res.end("already have");
+				// 여기서 안끝나고 자꾸 새로운 row 생성 -> 수정해야함
+			}
+
+			rid = rows[j].rid;
+			console.log("rid = " + rid);
+			if(rows[j].skinid_1 == null)
+			{
+				key1 = "skinid_1";
+				key2 = "gettime_1";
+				break;
+			}
+			else if(rows[j].skinid_2 == null)
+			{
+				key1 = "skinid_2"
+				key2 = "gettime_2";
+				break;
+			}
+			else if(rows[j].skinid_3 == null)
+			{
+				key1 = "skinid_3";
+				key2 = "gettime_3";
+				break;
+			}
+			else if(rows[j].skinid_4 == null)
+			{
+				key1 = "skinid_4";
+				key2 = "gettime_4";
+				break;
+			}
+			else if(rows[j].skinid_5 == null)
+			{
+				key1 = "skinid_5";
+				key2 = "gettime_5";
+				break;
+			}
+
+		}
+
+		if(key1 == "default")
+		{
+			connection.query('insert into skin(userid, skinid_1, gettime_1) values(?, ?, sysdate())', [userid, skinid], function(error, results)
+			{
+				console.log("insert");
+				if(error) {console.log(error);}
+				console.log(JSON.stringify(results));
+			});
+		}
+		else
+		{
+	        	connection.query('update skin set ' + key1 + '=?, ' + key2 + '=sysdate() where rid=?', [skinid, rid], function(error, results)
+        		{	
+				console.log("update");
+		                if(error){console.log(error);}
+				console.log(JSON.stringify(results));
+			});
+		}
+
+		connection.query('select * from skin where userid=?', userid, function(error, results)
+		{
+			console.log("select");
+			if(error) {console.log(error);}
+			res.end(JSON.stringify(results));
+		});
+	});
+});
