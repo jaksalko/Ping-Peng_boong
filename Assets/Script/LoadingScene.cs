31d03470851364bf85c956d3c248fd876324ab60
jaksalko <jaksal95@naver.com>
2021-01-03 15:50:49 +0900

    create aws xml

--- Assets/Script/LoadingScene.cs
+++ Assets/Script/LoadingScene.cs
@@ -13,6 +13,7 @@ using Amazon.CognitoIdentity;
 using Amazon.IdentityManagement;
 
 using Facebook.Unity;
+using UnityEngine.SignInWithApple;
 
 
 
@@ -32,6 +33,7 @@ public class LoadingScene : MonoBehaviour
     public Button play_button;
 
     CognitoAWSCredentials credentials;
+    private string userId;
 
 
     private void Awake()
@@ -41,6 +43,8 @@ public class LoadingScene : MonoBehaviour
         StartCoroutine(Interpolation());//Animation Effect
 
         UnityInitializer.AttachToGameObject(this.gameObject);
+        AWSConfigs.HttpClient = AWSConfigs.HttpClientOption.UnityWebRequest; 
+
         credentials = new CognitoAWSCredentials (
             "ap-northeast-2:1f71a893-368d-4067-8122-11b8dd2db2b4", // Identity Pool ID
             RegionEndpoint.APNortheast2 // Region : Seoul
@@ -51,7 +55,9 @@ public class LoadingScene : MonoBehaviour
 
     public void SignUpWithApple()
     {
-
+        var siwa = gameObject.GetComponent<SignInWithApple>();
+        siwa.Login(OnLogin);
+        
     }
     public void SignUpWithFacebook()
     {
@@ -85,11 +91,49 @@ public class LoadingScene : MonoBehaviour
     void AddFacebookTokenToCognito()
     {
         credentials.AddLogin ("graph.facebook.com", AccessToken.CurrentAccessToken.TokenString);
+        credentials.GetIdentityIdAsync(delegate(AmazonCognitoIdentityResult<string> result) {
+            if (result.Exception != null) {
+                //Exception!
+                Debug.Log("no identity id!");
+            }
+            string identityId = result.Response;
+            Debug.Log(identityId);
+        });
     }
+
+    private void OnLogin(SignInWithApple.CallbackArgs args)
+    {
+        if (args.error != null)
+        {
+            Debug.Log("Errors occurred: " + args.error);
+            return;
+        }
+
+        UserInfo userInfo = args.userInfo;
+        credentials.AddLogin("appleid.apple.com", userInfo.idToken);
+
+        // Save the userId so we can use it later for other operations.
+        userId = userInfo.userId;
+        var siwa = gameObject.GetComponent<SignInWithApple>();
+        siwa.GetCredentialState(userId, OnCredentialState);
+        
+        // Print out information about the user who logged in.
+        Debug.Log(
+            string.Format("Display Name: {0}\nEmail: {1}\nUser ID: {2}\nID Token: {3}", 
+            userInfo.displayName ?? "", userInfo.email ?? "", userInfo.userId ?? "", userInfo.idToken ?? ""));
+    }
+    private void OnCredentialState(SignInWithApple.CallbackArgs args)
+    {
+        Debug.Log(string.Format("User credential state is: {0}", args.credentialState));
+
+        if (args.error != null)
+            Debug.Log(string.Format("Errors occurred: {0}", args.error));
+    }
+
     /*void CloudInitializeCompleted()
     {
         Cloud.OnInitializeComplete -= CloudInitializeCompleted;
-
+    
       
 
         Debug.LogWarning("initialize completed");
